a4d3c58925bf9044d70d6908f070e4e09286634b
Sergey Zabelin <ns6630@gmail.com>
2020-10-12 21:11:33 +0300

    Реализовано автоматическое удаление старого изображения при его обновлении в слайде карусели.

--- carousel/models.py
+++ carousel/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.dispatch import receiver
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, post_save
 from PIL import Image
 import os
 
@@ -9,6 +9,10 @@ class CarouselSlide(models.Model):
     image = models.ImageField(verbose_name='Изображение', upload_to='carousel')
     is_active = models.BooleanField(verbose_name='Является ли активным', default=True)
     order = models.IntegerField(verbose_name='Номер слайда в карусели')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_image = self.image
 
     def save(self):        
         super(CarouselSlide, self).save()
@@ -31,3 +35,11 @@ def auto_delete_image_on_delete(sender, instance, **kwargs):
     if instance.image:
         if os.path.isfile(instance.image.path):
             os.remove(instance.image.path)
+
+
+@receiver(post_save, sender=CarouselSlide)
+def auto_delete_old_image_on_save(sender, instance, **kwargs):
+    if instance.original_image and instance.original_image.path:
+        if instance.original_image.path != instance.image.path:
+            if os.path.isfile(instance.original_image.path):
+                os.remove(instance.original_image.path)
